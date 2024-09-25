007e8e3770a2478e1ff4e5eeb5d9ead3cdd471da
Kareem Aljandali <W0840126@myscc.ca>
2024-09-25 10:30:30 -0400

    Assignment2 created datatype

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Team} from "./interFaces/team";
 
 // @ts-ignore
 @Component({
@@ -13,4 +14,7 @@ export class AppComponent {
   title = 'KareemAljandaliLearningAngular';
   name : string = 'Kareem Aljandali';
   number : string = '19.5';
+
+
 }
+
