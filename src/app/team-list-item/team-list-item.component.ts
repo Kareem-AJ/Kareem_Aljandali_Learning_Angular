61c1cf88c7fa5a1461871f6adc53bde41b4aa229
Kareem Aljandali <W0840126@myscc.ca>
2024-09-27 14:22:40 -0400

    Creating components

---
+++ src/app/team-list-item/team-list-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-team-list-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './team-list-item.component.html',
+  styleUrl: './team-list-item.component.css'
+})
+export class TeamListItemComponent {
+
+}
