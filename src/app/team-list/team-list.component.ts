096ecac4510f77576f7b99a4f99bd263c097a468
Kareem Aljandali <W0840126@myscc.ca>
2024-09-27 15:14:35 -0400

    Added Background color to the components based on index using ngClass

--- src/app/team-list/team-list.component.ts
+++ src/app/team-list/team-list.component.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core';
 import {Team} from "../interFaces/team";
 import {TeamListItemComponent} from "../team-list-item/team-list-item.component";
 import {style} from "@angular/animations";
+import {NgClass, NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-team-list',
   standalone: true,
   imports: [
     TeamListItemComponent,
+    NgForOf,
+    NgClass,
   ],
   templateUrl: './team-list.component.html',
   styleUrl: './team-list.component.css'
