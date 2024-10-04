17909a81f3efc4dce8fc176ca7b42c3031c26a1c
Kareem Aljandali <W0840126@myscc.ca>
2024-10-04 14:42:18 -0400

    Added CRUD methods for team

--- src/app/team-list/team-list.component.ts
+++ src/app/team-list/team-list.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Team} from "../interFaces/team";
 import {TeamListItemComponent} from "../team-list-item/team-list-item.component";
 import {style} from "@angular/animations";
 import {NgClass, NgForOf} from "@angular/common";
 import {TeamServiceService} from "../services/team-service.service";
+import {soccerTeams} from "../Data/mock-team";
+import {Observable, of} from "rxjs";
 
 @Component({
   selector: 'app-team-list',
@@ -16,8 +18,39 @@ import {TeamServiceService} from "../services/team-service.service";
   templateUrl: './team-list.component.html',
   styleUrl: './team-list.component.css'
 })
-export class TeamListComponent {
+export class TeamListComponent implements OnInit{
+  teamList: Team[] = [];
   constructor(private teamService: TeamServiceService) {
   }
-  protected readonly style = style;
+
+  ngOnInit(): void {
+    this.teamService.getTeams().subscribe({
+      next: (data: Team[]) => this.teamList = data
+    })
+  }
+  addTeam(newStudent:Team) : Observable<Team[]>{
+    this.teamList.push(newStudent)
+    return of(this.teamList);
+  }
+//Update an Existing user
+  updateTeam(updatedTeam: Team): Observable<Team[]> {
+    const index = this.teamList.findIndex(team => team.id === updatedTeam.id);
+    if (index !== -1) {
+      this.teamList[index] = updatedTeam;
+    }
+    return of(this.teamList);
+  }
+//Delete: Remove a user by ID
+  deleteTeam(teamId: number): Observable<Team[]> {
+    this.teamList = this.teamList.filter(team => team.id !== teamId);
+    return of(this.teamList);
+  }
+//Get team by ID
+  getTeamById(teamId: number): Observable<Team | undefined> {
+    const student = this.teamList.find(team => team.id === teamId);
+    return of(student);
+  }
+
+
+  protected readonly soccerTeams = soccerTeams;
 }
