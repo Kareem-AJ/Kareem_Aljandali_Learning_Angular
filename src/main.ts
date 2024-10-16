90c051af50e1f045951beec2df1e25f03c927488
Kareem Aljandali <W0840126@myscc.ca>
2024-10-16 14:25:00 -0400

    Adding the team list component to the main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
+import {provideRouter, Routes} from "@angular/router";
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {TeamListComponent} from "./app/team-list/team-list.component";
+import {TeamListItemComponent} from "./app/team-list-item/team-list-item.component";
 
+
+const routes: Routes = [
+  { path: 'teams', component: TeamListComponent},
+  { path: 'teams/:id', component: TeamListItemComponent},
+]
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
